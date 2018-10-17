6ce9136e4bfd5947c3355c39b552321922e7723b
Dendi Handian <handian.dendi@gmail.com>
2018-10-18 00:56:16 +0700

    add link to filterTweets

--- app/helper.js
+++ app/helper.js
@@ -3,13 +3,17 @@ import moment from 'moment'
 const filterTweets = async (tweets) => {
   let filteredTweets = []
   tweets.forEach((tweet) => {
+    const arr_created_at = tweet.created_at.split(" ")
+    const createdAt = `${arr_created_at[5]}-${moment().month(arr_created_at[1]).format('M')}-${arr_created_at[2]} ${arr_created_at[3]}`
     filteredTweets.push({
       id: tweet.id,
       text: tweet.text,
-      date: moment(tweet.created_at).format('Y-M-D HH:mm:ss')
+      link: `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`,
+      createdAt: createdAt
     })
   })
   return filteredTweets
 }
+
 
 module.exports = { filterTweets }
